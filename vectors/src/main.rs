7f03ba245fa6012009e06aef6ded31f0ab7cbb9a
kingdido999 <kingdido999@gmail.com>
2017-12-18 16:33:18 -0800

    Add vectors

---
+++ vectors/src/main.rs
@@ -0,0 +1,40 @@
+fn main() {
+    let v: Vec<i32> = Vec::new();
+
+    // With type inference
+    let v = vec![1, 2, 3];
+
+    let mut v = Vec::new();
+    v.push(5);
+    v.push(6);
+    v.push(7);
+    v.push(8);
+
+    let v = vec![1, 2, 3, 4, 5];
+
+    let third: &i32 = &v[2];
+    let third: Option<&i32> = v.get(2);
+
+    let v = vec![100, 32, 57];
+    for i in &v {
+        println!("{}", i);
+    }
+
+    let mut v = vec![100, 32, 57];
+    for i in &mut v {
+        *i += 50;
+        println!("{}", i);
+    }
+
+    enum SpreadsheetCell {
+        Int(i32),
+        Float(f64),
+        Text(String)
+    }
+
+    let row = vec![
+        SpreadsheetCell::Int(3),
+        SpreadsheetCell::Text(String::from("blue")),
+        SpreadsheetCell::Float(10.12)
+    ];
+}
