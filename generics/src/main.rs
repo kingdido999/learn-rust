0b0107a061eddf5652fbb2adf6d49126071783f8
kingdido999 <kingdido999@gmail.com>
2017-12-19 20:27:52 -0800

    Update largest fn to use trait bound

--- generics/src/main.rs
+++ generics/src/main.rs
@@ -1,28 +1,6 @@
-fn largest_i32(list: &[i32]) -> i32 {
-    let mut largest = list[0];
+use std::cmp::PartialOrd;
 
-    for &item in list.iter() {
-        if item > largest {
-            largest = item;
-        }
-    }
-
-    largest
-}
-
-fn largest_char(list: &[char]) -> char {
-    let mut largest = list[0];
-
-    for &item in list.iter() {
-        if item > largest {
-            largest = item;
-        }
-    }
-
-    largest
-}
-
-fn largest<T>(list: &[T]) -> T {
+fn largest<T: PartialOrd + Copy>(list: &[T]) -> T {
     let mut largest = list[0];
 
     for &item in list.iter() {
